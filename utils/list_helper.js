ca6f09dd46bb39cc99311138ab4ff9dbc8bd48fd
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2023-01-05 20:24:54 -0700

    Finish exercises 4.6 and 4.7

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const dummy = () => 1;
 
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0);
@@ -11,8 +13,32 @@ const favoriteBlog = ((blogs) => {
   );
 });
 
+const mostBlogs = (blogs) => {
+  const count = _.countBy(blogs, (blog) => blog.author);
+  return {
+    author: _.maxBy(_.keys(count), (o) => count[o]),
+    blogs: _.maxBy(_.values(count), (o) => o),
+  };
+};
+
+const mostLikes = (blogs) => {
+  const grouped = _.groupBy(blogs, (blog) => blog.author);
+
+  const likeCount = {};
+  _.forEach(grouped, (authorBlogs, author) => {
+    likeCount[author] = _.reduce(authorBlogs, (result, value) => result + value.likes, 0);
+  });
+
+  return {
+    author: _.maxBy(_.keys(likeCount), (o) => likeCount[o]),
+    likes: _.maxBy(_.values(likeCount), (o) => o),
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
