0bd4828f181e3f98c67b670df190d294cb30e0ee
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2023-01-09 12:29:34 -0700

    Finish exercises 4.17 to 4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,21 +1,32 @@
 const blogRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 const Blog = require('../models/blog');
+const User = require('../models/user');
+const { getTokenFrom } = require('../utils/blogs_helper');
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog
+    .find({})
+    .populate('user');
   response.json(blogs);
 });
 
 blogRouter.post('/', async (request, response) => {
   console.log(request.body);
 
-  // if title or url are missing, return with status 400 Bad Request
-  if (!Object.hasOwn(request.body, 'title') || !Object.hasOwn(request.body, 'url')) { response.status(400).end(); } else {
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!decodedToken.id) {
+    response.status(401).json({ error: 'token missing or invalid' });
+  } else if (!Object.hasOwn(request.body, 'title') || !Object.hasOwn(request.body, 'url')) { response.status(400).end(); } else {
     // if likes was not provided, set likes to 0
     if (!Object.hasOwn(request.body, 'likes')) { request.body.likes = 0; }
+    const user = await User.findById(decodedToken.id);
 
-    const blog = new Blog(request.body);
+    const blog = new Blog({ ...request.body, user: user._id });
     const result = await blog.save();
+    user.blogs = user.blogs.concat(result._id);
+    await user.save();
 
     response.status(201).json(result);
   }
--- utils/config.js
+++ utils/config.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 const { PORT } = process.env;
 const { MONGODB_URI } = process.env;
 const { TEST_MONGODB_URI } = process.env;
+const { SECRET } = process.env;
 
 module.exports = {
   MONGODB_URI,
   TEST_MONGODB_URI,
   PORT,
+  SECRET,
 };
